801be270c78cbd91067aed8aadd6a09f5dcfbd91
devrishavjha <devrishavjha@gmail.com>
2025-10-23 01:04:58 +0530

    Initial commit of frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // remove /auth
+  baseURL: "https://gym-pro-jz9w.onrender.com/", // remove /auth
 });
 
 // Add token to headers if available
